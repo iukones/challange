3845ee6d3ca86ed614888f6bfde2fa05a5622895
iukones <iukones1@gmail.com>
2022-02-16 19:08:08 -0600

    edit background login

--- pages/login.js
+++ pages/login.js
@@ -6,26 +6,34 @@ import Image from 'next/image';
 import styles from '../styles/Login.module.css'
 
 const Login = () => {
+	const url = '';
 
   return (
     <section className={styles.login}>
 		<div className={styles.login_box}>
 			<div className={styles.left}>
-				<div className={styles.top_link}><a href="#">
-          <Image className={styles.image} src="/vercel.svg" alt="Vercel Logo" width={100} height={16} />Return home</a></div>
+				<div className={styles.top_link}>
+					<a href="#">
+						<Image 
+						className={styles.image} 
+						src="/vercel.svg" alt="Vercel Logo" 
+						width={100} height={16} />
+						No tienes usuario, registrate
+					</a>
+				</div>
 				<div className={styles.contact}>
 					<form >
-						<h3>SIGN IN</h3>
-						<input type="text" placeholder="USERNAME" />
-						<input type="text" placeholder="PASSWORD" />
-						<button className={styles.submit}>LET'S GO</button>
+						<h3>Conéctate aquí</h3>
+						<input type="text" placeholder="Usuario" />
+						<input type="password" placeholder="Contraseña" />
+						<button className={styles.submit}>Inicia sesión</button>
 					</form>
 				</div>
 			</div>
 			<div className={styles.right}>
 				<div className={styles.rightText}>
-					<h2>LONYX</h2>
-					<h5>A UX BASED CREATIVE AGENCEY</h5>
+					<h2>El software para los restaurantes de hoy.</h2>
+					<h5>Tu restaurante está listo para llegar más alto.</h5>
 				</div>
 				<div className={styles.rightInductor}><Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /></div>
 			</div>
